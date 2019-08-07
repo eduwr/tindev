ad18f12099035aeb3759f33da514ffe762f2d881
eduardo <eduwric@gmail.com>
2019-08-07 17:56:42 -0300

    frontend - 1

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+
+export default function Login() {
+    return(
+        <div className="login-container">
+            <img src={logo} alt="Tindev" />
+        </div>
+    );
+}
