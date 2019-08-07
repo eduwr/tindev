614347d8729fa1b0a79ea9febf4c978c508c7d35
eduardo <eduwric@gmail.com>
2019-08-07 15:56:25 -0300

    Backend do tindev

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-jwyto.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
